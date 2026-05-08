644da1d7b913fd2438cab806294644e8acea6494
VishnuKumarRD <vishnukumarvkrd@gmail.com>
2026-05-08 08:02:46 +0530

    Java + Jdbc

---
+++ src/main/java/com/atm/IAtm.java
@@ -0,0 +1,17 @@
+package com.atm;
+
+public interface IAtm {
+	void createAccount();
+	
+	void depositAmount();
+	
+	void withdrawAmount();
+	
+	void checkBalance();
+	
+	void accountUpdation();
+	
+	void accountDetails();
+	
+	void accountDeletion();
+}
